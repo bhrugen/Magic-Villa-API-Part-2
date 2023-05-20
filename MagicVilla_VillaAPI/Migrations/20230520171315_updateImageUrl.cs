2145e1c91f102104017cd55360e57d5db80ca238
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-05-20 12:15:00 -0500

    Section 2 - Load Placehold Image

---
+++ MagicVilla_VillaAPI/Migrations/20230520171315_updateImageUrl.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x401");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x402");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x403");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x404");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x405");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://dotnetmastery.com/bluevillaimages/villa3.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://dotnetmastery.com/bluevillaimages/villa1.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://dotnetmastery.com/bluevillaimages/villa4.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://dotnetmastery.com/bluevillaimages/villa5.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "https://dotnetmastery.com/bluevillaimages/villa2.jpg");
+        }
+    }
+}
