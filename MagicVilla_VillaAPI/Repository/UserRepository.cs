5d9dce727511cdd36b1ea9d75982fd960da40e8d
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-05-23 05:51:57 -0500

    Section 4 - Seperating out Access Token Generator

--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -55,26 +55,11 @@ namespace MagicVilla_VillaAPI.Repository
                 };
             }
 
-            //if user was found generate JWT Token
-            var roles = await _userManager.GetRolesAsync(user);
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(secretKey);
+            var accessToken = await GetAccessToken(user);
 
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, user.UserName.ToString()),
-                    new Claim(ClaimTypes.Role, roles.FirstOrDefault())
-                }),
-                Expires = DateTime.UtcNow.AddDays(7),
-                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-
-            var token = tokenHandler.CreateToken(tokenDescriptor);
             TokenDTO tokenDto = new TokenDTO()
             {
-                AccessToken = tokenHandler.WriteToken(token),
+                AccessToken = accessToken,
                 
             };
             return tokenDto;
@@ -111,6 +96,29 @@ namespace MagicVilla_VillaAPI.Repository
             }
 
             return new UserDTO();
+        }
+
+        private async Task<string> GetAccessToken(ApplicationUser user)
+        {
+            //if user was found generate JWT Token
+            var roles = await _userManager.GetRolesAsync(user);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.UserName.ToString()),
+                    new Claim(ClaimTypes.Role, roles.FirstOrDefault())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenStr = tokenHandler.WriteToken(token);
+            return tokenStr;
         }
     }
 }
