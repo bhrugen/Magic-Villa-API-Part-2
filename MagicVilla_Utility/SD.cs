28de97dac5b4466764c23473d451cdb1fe58fd2c
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-05-23 06:34:49 -0500

    Section 5 - Add Refresh Token to Token Provider

--- MagicVilla_Utility/SD.cs
+++ MagicVilla_Utility/SD.cs
@@ -10,6 +10,7 @@
             DELETE
         }
         public static string AccessToken = "JWTToken";
+        public static string RefreshToken = "RefreshToken";
         public static string CurrentAPIVersion = "v2";
         public const string Admin = "admin";
         public const string Customer = "customer";
--- MagicVilla_Web/Services/TokenProvider.cs
+++ MagicVilla_Web/Services/TokenProvider.cs
@@ -17,6 +17,7 @@ namespace MagicVilla_Web.Services
         public void ClearToken()
         {
             _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.AccessToken);
+            _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.RefreshToken);
         }
 
         public TokenDTO GetToken()
@@ -24,9 +25,12 @@ namespace MagicVilla_Web.Services
             try
             {
                 bool hasAccessToken = _contextAccessor.HttpContext.Request.Cookies.TryGetValue(SD.AccessToken, out string accessToken);
+                bool hasRefreshToken = _contextAccessor.HttpContext.Request.Cookies.TryGetValue(SD.RefreshToken, out string refreshToken);
+
                 TokenDTO tokenDTO = new()
                 {
-                    AccessToken= accessToken
+                    AccessToken= accessToken,
+                    RefreshToken= refreshToken
                 };
                 return hasAccessToken ? tokenDTO : null;
             }
@@ -40,6 +44,7 @@ namespace MagicVilla_Web.Services
         {
             var cookieOptions = new CookieOptions { Expires = DateTime.UtcNow.AddDays(60) };
             _contextAccessor.HttpContext?.Response.Cookies.Append(SD.AccessToken,tokenDTO.AccessToken, cookieOptions);
+            _contextAccessor.HttpContext?.Response.Cookies.Append(SD.RefreshToken, tokenDTO.RefreshToken, cookieOptions);
         }
     }
 }
