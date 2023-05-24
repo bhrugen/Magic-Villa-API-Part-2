35fe6106a4881ec58296f69f5e2882fe37befa32
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-05-24 17:27:01 -0500

    Custom Error Handler in Program.cs

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -14,6 +14,9 @@ using Microsoft.AspNetCore.Identity;
 using MagicVilla_VillaAPI.Models;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -88,7 +91,24 @@ app.UseSwagger();
 // Configure the HTTP request pipeline.
 
 //this will show a different exception
-app.UseExceptionHandler("/ErrorHandling/ProcessErrorDev");
+//app.UseExceptionHandler("/ErrorHandling/ProcessErrorDev");
+app.UseExceptionHandler(error =>
+{
+    error.Run(async context =>
+    {
+        context.Response.StatusCode = 777;
+        context.Response.ContentType = "application/json";
+        var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+        if (contextFeature != null)
+        {
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+            {
+                StatusCode = context.Response.StatusCode,
+                ErrorMessage = "Hello From Middleware!"
+            }));
+        }
+    });
+});
 if (app.Environment.IsDevelopment())
 {
     app.UseSwaggerUI(options => {
