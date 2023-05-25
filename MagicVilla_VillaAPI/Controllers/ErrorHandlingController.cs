b134e519f057e0f183d3c85984f0419f26d8dd83
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-05-25 08:36:12 -0500

    Section 9 - Custom Problem Implementation

--- MagicVilla_VillaAPI/Controllers/ErrorHandlingController.cs
+++ MagicVilla_VillaAPI/Controllers/ErrorHandlingController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,23 @@ namespace MagicVilla_VillaAPI.Controllers
     public class ErrorHandlingController : ControllerBase
     {
         [Route("ProcessError")]
-        public IActionResult ProcessError() => Problem();
+        public IActionResult ProcessError([FromServices] IHostEnvironment hostEnvironment)
+        {
+            if (hostEnvironment.IsDevelopment())
+            {
+                //custom logic
+                var feature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+                return Problem(
+                    detail: feature.Error.StackTrace,
+                    title: feature.Error.Message,
+                    instance: hostEnvironment.EnvironmentName
+                    );
+            }
+            else
+            {
+                return Problem();
+            }
+        } 
     }
 }
